4d9cf16c3d985bc475cbe90365d392d28236f01a
Sasha Nicholai Rosenthal <sar537@g.harvard.edu>
2018-07-23 21:03:13 +0000

    finished all the functionality

--- application.py
+++ application.py
@@ -1,15 +1,46 @@
 import os
 import requests
+import json
 
 from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
+import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+
 # list of all channels
-channel_list = ['general']
+channel_list = {}
+channel_list['general'] = []
+
+#sets up storage for messages
+channel_list['general'].append('Chat Bot said "Welcome to ChatterBot!" at 14:52 on 7/23')
+
+@socketio.on("create message")
+def message(channel,message):
+    channel_list[f"{channel['channel']}"].append(message['message'])
+    if len(channel_list[f"{channel['channel']}"]) > 100:
+        del channel_list[f"{channel['channel']}"][0]
+        emit("new message", channel_list[f"{channel['channel']}"][100], broadcast=True)
+    value = len(channel_list[f"{channel['channel']}"])
+    emit("new message", channel_list[f"{channel['channel']}"][value-1], broadcast=True)
+
+@socketio.on("connecting")
+def connect():
+    channel = json.dumps(channel_list)
+    emit("connected", channel, broadcast=False)
+
+@socketio.on("channel change")
+def message_load():
+    channel = json.dumps(channel_list)
+    emit("message load", channel, broadcast=False)
+
+@socketio.on("create channel")
+def channel(channel):
+    channel_list[f"{channel['channel']}"] = []
+    emit("new channel", f"{channel['channel']}", broadcast=True)
 
 @app.route("/")
 def index():
--- static/index.js
+++ static/index.js
@@ -3,46 +3,110 @@ document.addEventListener('DOMContentLoaded', () => {
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    // When connected, configure stuff
-    socket.on('connect', () =>
-    {
-
+    socket.on('connect', () => {
+        var name = localStorage.getItem('name');
+        if (name == 'null')
+        {
+            localStorage.setItem('name','not set');
+        }
+        currentChannel = localStorage.getItem('channel');
+        if (currentChannel == 'null')
+        {
+            localStorage.setItem('channel','general');
+        }
+        if (currentChannel == '[object Object]')
+        {
+            localStorage.setItem('channel','general');
+        }
+        document.querySelector('#name').append(name);
+        socket.emit('connecting');
     });
 
-
-    if (!localStorage.getItem('name'))
+    // When connected, configure stuff
+    socket.on('connected', channel =>
     {
-        var name = prompt("Please enter your username");
-        localStorage.setItem('name',name);
-    }
+        document.querySelector('#channel-change').innerHTML = '';
+        channels = JSON.parse( channel );
+        var key = Object.keys(channels);
+        for (var i = 0 ; i < key.length ; i++)
+        {
+            const option = document.createElement('option');
+            option.innerHTML = key[i];
+            option.value = key[i];
+            document.querySelector('#channel-change').append(option);
+        }
+        channel = localStorage.getItem('channel');
+        socket.emit('channel change');
+    });
+
+    socket.on('message load', channel =>
+    {
+
+        channels = JSON.parse(channel);
+        currentChannel = localStorage.getItem('channel');
+        var data = channels[currentChannel];
+        document.querySelector('#messages').innerHTML = '';
+        for (var i =0;i < data.length ;i++)
+        {
+            const li = document.createElement('li');
+            li.innerHTML = data[i];
+            document.querySelector('#messages').append(li);
+        }
+    });
 
     document.querySelector('.channel').onsubmit = function() {
                     const channel = document.querySelector('#channelName').value;
                     socket.emit('create channel', {'channel': channel});
+                    document.querySelector('#channelName').value = '';
+                    return false;
                 };
+
+    document.querySelector('#channel-change').onchange = function() {
+                    localStorage.setItem('channel', this.value);
+                    channel = localStorage.getItem('channel');
+
+                    socket.emit('channel change');
+                };
+
+    document.querySelector('.name').onsubmit = function() {
+        var name = document.querySelector('#changeName').value;
+        localStorage.setItem('name', name);
+        name = localStorage.getItem('name');
+        document.querySelector('#name').innerHTML = name;
+        document.querySelector('#changeName').value = '';
+        return false;
+    };
 
     document.querySelector('.message').onsubmit = function() {
             var date = new Date();
-            name = localStorage.getItem('name');
-            const message = document.querySelector('#messageSent').value;
-            socket.emit('create message', {'message': message},{'date':date},{'name':name});
+            var name = localStorage.getItem('name');
+            var message = document.querySelector('#messageSent').value;
+            const channel = localStorage.getItem('channel');
+            message = name + ' said "' + message + '" at ' + date.getHours() + ':' + date.getMinutes() + ' on ' + (date.getMonth()+1) + '/' + date.getDate();
+
+            socket.emit('create message',{'channel':channel}, {'message': message});
+            document.querySelector('#messageSent').value = '';
+            return false;
         };
     /*
     var date = new Date(); // for now
         date.getHours(); // => 9
         date.getMinutes(); // =>  30
         date.getSeconds(); // => 51
-
-    socket.emit('create message', {'message': messaage},{'date':date},{'name':name});
     */
 
-    socket.on('new channel', placeHolder =>
+    socket.on('new channel', channel =>
     {
-
+        const option = document.createElement('option');
+        option.innerHTML = channel;
+        option.id = channel;
+        document.querySelector('#channel-change').append(option);
     });
 
-    socket.on('new message', placeHolder =>
+    socket.on('new message', message =>
     {
-
+        const li = document.createElement('li');
+        li.innerHTML = message;
+        document.querySelector('#messages').append(li);
     });
 });
